35b5efe6c42f967f9c932d56eba04eaf590cecd0
igorcruzz <igorcruz.dev@gmail.com>
2025-03-25 20:47:33 -0300

    feat: add event to get events length

--- notify-process/app.ts
+++ notify-process/app.ts
@@ -4,6 +4,8 @@ import { SnsService } from './services/sns-services';
 import { DynamoDBService } from './services/dynamodb-services';
 
 export const handler = async (event: SQSEvent): Promise<void> => {
+    console.log('Events received -> ', event.Records.length);
+
     for (const record of event.Records) {
         try {
             const body = JSON.parse(record.body);
