ae5121eeac060917ce383c6788c2554e9f48359c
igorcruzz <igorcruz.dev@gmail.com>
2025-03-25 23:02:21 -0300

    refactor: remove batch publish on SNS

--- notify-validation/services/sns-services.ts
+++ notify-validation/services/sns-services.ts
@@ -1,55 +1,25 @@
-import {
-    PublishCommand,
-    PublishCommandInput,
-    SNSClient,
-    PublishBatchCommandInput,
-    PublishBatchCommand,
-} from '@aws-sdk/client-sns';
+import { PublishCommand, PublishCommandInput, SNSClient } from '@aws-sdk/client-sns';
 import { NotifyType } from '../types';
 
 const snsClient = new SNSClient({});
 
 export const NotifyRepository = {
     publishMessage: async (data: NotifyType) => {
-        const publishBatchCommandInput: PublishBatchCommandInput = {
+        const publishCommandInput: PublishCommandInput = {
             TopicArn: process.env.SNS_TOPIC_ARN,
-            PublishBatchRequestEntries: Array.from({ length: 10 }, (_, index) => ({
-                Id: `message-${index}`,
-                Message: JSON.stringify({
-                    userId: index,
-                    message: data.message,
-                    priority: data.priority,
-                }),
-                MessageAttributes: {
-                    priority: {
-                        DataType: 'String',
-                        StringValue: data.priority,
-                    },
+            Message: JSON.stringify({
+                userId: data.userId,
+                message: data.message,
+                priority: data.priority,
+            }),
+            MessageAttributes: {
+                priority: {
+                    DataType: 'String',
+                    StringValue: data.priority,
                 },
-            })),
+            },
         };
-
-        const publishCommand = new PublishBatchCommand(publishBatchCommandInput);
-
+        const publishCommand = new PublishCommand(publishCommandInput);
         await snsClient.send(publishCommand);
-
-        // const publishCommandInput: PublishCommandInput = {
-        //     TopicArn: process.env.SNS_TOPIC_ARN,
-        //     Message: JSON.stringify({
-        //         userId: data.userId,
-        //         message: data.message,
-        //         priority: data.priority,
-        //     }),
-        //     MessageAttributes: {
-        //         priority: {
-        //             DataType: 'String',
-        //             StringValue: data.priority,
-        //         },
-        //     },
-        // };
-
-        // const publishCommand = new PublishCommand(publishCommandInput);
-
-        // await snsClient.send(publishCommand);
     },
 };
